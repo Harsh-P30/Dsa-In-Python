f800270ffc10e4a2a554ef8c58b0413cea928740
Harsh Prasad <harshprasad1204@gmail.com>
2026-01-19 22:56:48 +0530

    using hashing to calculate frequence of each element and return high frequency elememt

---
+++ 19-1-2026/freq_count.py
@@ -0,0 +1,21 @@
+# we are using hashing to calculate frequence of each element and then going to return high and low frequency element.
+
+
+from collections import defaultdict
+
+
+arr = [1,2,3,1,23,12,4,4,233,12,2,3,3,3,3,33]
+freq=defaultdict(int)
+for i in arr:
+    freq[i]+=1
+
+high=0
+high_val=0
+low=max
+for key in freq:
+    if(high<freq[key]):
+        high=freq[key]
+        high_val=key
+        
+print(high)
+print(high_val)
