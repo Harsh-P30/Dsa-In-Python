d33b8cd9c4e8061319ac6ebf29b6af68fd6261fb
Harsh Prasad <harshprasad1204@gmail.com>
2026-02-18 08:53:13 +0530

    write notes

--- 19-1-2026/freq_count.py
+++ 19-1-2026/freq_count.py
@@ -1,5 +1,7 @@
 # we are using hashing to calculate frequence of each element and then going to return high and low frequency element.
-
+# diff bt dict and hashmap in python -> 
+    # you can use string index into hashmap/hashtable.
+    # Hashtable/hashmap is internal data structure while dictonary is a pyhon specific implementaion of hashtable.
 
 from collections import defaultdict
 
